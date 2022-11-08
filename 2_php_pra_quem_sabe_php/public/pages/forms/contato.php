949d5dad521562f0a58bf099df06651353e1adc5
Carlos-Daniel-Dev <carlos.daniel.brathon2@gmail.com>
2022-11-08 19:32:57 -0300

    firts commit

---
+++ 2_php_pra_quem_sabe_php/public/pages/forms/contato.php
@@ -0,0 +1,16 @@
+<?php   
+
+require "../../../bootstrap.php";
+
+dd($_SERVER['REQUEST_METHOD']);
+
+  $validate = validate([
+    'name' => 's',
+    'email' => 'e',
+    'subject' => 's',
+    'message' => 's',
+
+  ]);
+
+  dd($validate->email);
+
